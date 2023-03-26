f16b804348da31cde3f1661b7c2ae29d3f699118
OlivierBinette <olivier.binette@gmail.com>
2023-03-26 10:02:04 -0400

    update license classifier

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache License 2.0 (Apache-2.0)",
+        "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
