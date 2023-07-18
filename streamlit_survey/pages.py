08edb72aadd867add436e66dddb0c1d5091aaa33
Olivier Binette <olivier.binette@gmail.com>
2023-07-18 19:55:55 -0400

    Add pages button customization

--- streamlit_survey/pages.py
+++ streamlit_survey/pages.py
@@ -4,9 +4,37 @@ import streamlit as st
 
 
 class Pages(object):
-    def __init__(self, labels: Union[int, list], key="__Pages_curent",
-                 on_submit=None, progress_bar=True,
-                 disable_next: bool = False):
+    DEFAULT_PREV_BUTTON = lambda pages: st.button(
+        "Previous",
+        use_container_width=True,
+        disabled=pages.current == 0,
+        on_click=pages.previous,
+        key=f"{pages.current_page_key}_btn_prev",
+    )
+
+    DEFAULT_NEXT_BUTTON = lambda pages: st.button(
+        "Next",
+        type="primary",
+        use_container_width=True,
+        on_click=pages.next,
+        disabled=pages.current == pages.n_pages - 1,
+        key=f"{pages.current_page_key}_btn_next",
+    )
+
+    DEFAULT_SUBMIT_BUTTON = lambda pages: st.button(
+        "Submit",
+        type="primary",
+        use_container_width=True,
+        key=f"{pages.current_page_key}_btn_submit",
+    )
+
+    def __init__(
+        self,
+        labels: Union[int, list],
+        key="__Pages_curent",
+        on_submit=None,
+        progress_bar=True
+    ):
         """
         Parameters
         ----------
@@ -16,8 +44,6 @@ class Pages(object):
             Key to use to store the current page in Streamlit's session state
         on_submit: Callable
             Callback to call when the user clicks the submit button
-        disable_next: bool
-            Whether to have the "Next" button disabled by default
 
         Example
         -------
@@ -35,7 +61,10 @@ class Pages(object):
         self.current_page_key = key
         self.on_submit = on_submit
         self.progress_bar = progress_bar
-        self.disable_next = disable_next
+
+        self._prev_btn = Pages.DEFAULT_PREV_BUTTON
+        self._next_btn = Pages.DEFAULT_NEXT_BUTTON
+        self._submit_btn = Pages.DEFAULT_SUBMIT_BUTTON
 
     def update(self, value):
         """
@@ -96,6 +125,63 @@ class Pages(object):
         if self.current < self.n_pages - 1:
             self.current += 1
 
+    @property
+    def prev_button(self):
+        """
+        Returns "previous" button for page navigation.
+        """
+        return self._prev_btn(self)
+
+    @prev_button.setter
+    def prev_button(self, func):
+        """
+        Set "previous" button for page navigation.
+
+        Parameters
+        ----------
+        func: function
+            Function taking one argument (the current page instance) and returning the "previous" button for page navigation.
+        """
+        self._prev_btn = func
+    
+    @property
+    def next_button(self):
+        """
+        Returns "next" button for page navigation.
+        """
+        return self._next_btn(self)
+
+    @next_button.setter
+    def next_button(self, func):
+        """
+        Set "next" button for page navigation.
+
+        Parameters
+        ----------
+        func: function
+            Function taking one argument (the current page instance) and returning the "next" button for page navigation.
+        """
+        self._next_btn = func
+    
+    @property
+    def submit_button(self):
+        """
+        Returns "submit" button for page navigation.
+        """
+        return self._submit_btn(self)
+
+    @submit_button.setter
+    def submit_button(self, func):
+        """
+        Set "submit" button for page navigation.
+
+        Parameters
+        ----------
+        func: function
+            Function taking one argument (the current page instance) and returning the "submit" button for page navigation.
+        """
+        self._submit_btn = func
+
     def __enter__(self):
         return self
 
@@ -106,31 +192,13 @@ class Pages(object):
         submitted = False
         left, _, right = st.columns([2, 4, 2])
         with left:
-            st.button(
-                "Previous",
-                use_container_width=True,
-                disabled=self.current == 0,
-                on_click=self.previous,
-                key=f"{self.current_page_key}_btn_prev",
-            )
+            self.prev_button
         with right:
             if self.current == self.n_pages - 1 and self.on_submit is not None:
-                submitted = st.button(
-                    "Submit",
-                    type="primary",
-                    use_container_width=True,
-                    key=f"{self.current_page_key}_btn_submit",
-                )
+                submitted = self.submit_button
             else:
-                st.button(
-                    "Next",
-                    type="primary",
-                    use_container_width=True,
-                    on_click=self.next,
-                    disabled=self.current == self.n_pages - 1 or self.disable_next,
-                    key=f"{self.current_page_key}_btn_next",
-                )
-        if self.progress_bar:
-            st.progress(self.current / (self.n_pages-1))
+                self.next_button
+        if self.progress_bar and self.n_pages > 1:
+            st.progress(self.current / (self.n_pages - 1))
         if submitted:
             self.on_submit()
